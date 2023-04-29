e637f87c81e9f88ca721dfaf5795441023767734
SoftwareEngineering <KKGB>
2023-04-29 10:18:29 -0700

    function 4,5,6 complete

--- main.c
+++ main.c
@@ -44,17 +44,31 @@ void filetime1(){
 
 //파일 2의 시간 정보를 가져오는 함수 작성
 void filetime2(){
-    
+    time2 = localtime(&stat2.st_mtime);    
 }
 
 //두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp(){
-    
+    printf("size compare\n");
+
+    if(stat1.st_size > stat2.st_size)
+        printf("text1 is bigger\n\n");
+    else if (stat1.st_size < stat2.st_size)
+        printf("text2 is bigger\n\n");
+    else
+        printf("sizes are equal\n\n");
 }
 
 //두 개의 파일 블락 수를 비교하는 함수 작성
 void blockcmp(){
-    
+    printf("block compare\n");
+
+    if((int)stat1.st_blocks > (int)stat2.st_blocks)
+        printf("text1 is more than text2\n\n");
+    else if((int)stat1.st_blocks < (int)stat2.st_blocks)
+        printf("text2 is more than text1\n\n");
+    else
+        printf("blocks are equal\n\n");    
 }
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성

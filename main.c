5da0e884db4aa9bc85ff68a13100507b19f58e3d
kimdoyeon817 <90391956+kimdoyeon817@users.noreply.github.com>
2023-05-02 17:43:40 +0900

    Update main.c

--- main.c
+++ main.c
@@ -76,19 +76,19 @@ void datecmp(){
     printf("date compare\n");
 
     if(time1->tm_mon > time2->tm_mon){
-	printf("text2 is early\n");
+	printf("text2 is early\n\n");
     }
     else if(time1->tm_mon < time2->tm_mon){
-	printf("text1 is early\n");
+	printf("text1 is early\n\n");
     }
     else if(time1->tm_mday > time2->tm_mday){
-	printf("text2 is early\n");
+	printf("text2 is early\n\n");
     }
     else if(time1->tm_mday < time2->tm_mday){
-	printf("text1 is early\n");
+	printf("text1 is early\n\n");
     }
     else{
-	printf("same date\n");
+	printf("same date\n\n");
     }
 }
 
@@ -103,6 +103,6 @@ void timecmp(){
 	printf("text2 is early\n");
     }
     else{
-	printf("same time"\n);
+	printf("same time\n");
     }
 }
